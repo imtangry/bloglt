b4fc7ab86c08f0d67994fb8adea2badd6e6dc48a
汤瑞宇 <tang.rui.yu@foxmail.com>
2017-07-22 16:44:17 +0800

    按钮点击多次，数据重复加载的bug修复

--- src/main/webapp/bloglt-static-file/js/load_more_indexArticle.js
+++ src/main/webapp/bloglt-static-file/js/load_more_indexArticle.js
@@ -1,5 +1,15 @@
 // 顺带解决载入文章的按钮？
+//生成short内容，试验中
+function load_short(){
+    var fulldiv=$(".full-data");
+    var shortdata=$(".short-data")
+}
+//防止按钮被点击多次，重复加载数据
+var flag=true;
+//点击加载更多
 $(".load-more").click(function () {
+if(flag){
+    flag=false;
     $(".load-more").text("( *・ω・)✄╰ひ╯,呀吼！正在加载哩...");
     var href = $(".article_more").last().attr("href");
     var param = 'articleId='+href.match(/articleId=(\S*)(&)?/)[1];
@@ -10,14 +20,20 @@ $(".load-more").click(function () {
         success:function(data){
             console.log(data);
             if(data.length==0){
-                setTimeout('$(".load-more\").text(\"没有更多喽...-________-\'\'")',400)
+                setTimeout(function(){
+                    $(".load-more").text("没有更多喽...-________-\'\'");
+                    flag=true;
+                },400)
             }else{
                 setTimeout(function(){
                     $("#body").append(data);
                     $(".load-more").text("加载更多...");
+                    flag=true;
                 },400)
 
             }
         }
     });
+}
+
 })
